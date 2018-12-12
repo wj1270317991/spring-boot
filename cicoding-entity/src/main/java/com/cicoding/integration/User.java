0055009bb47eb62536d5e0653e50f41ea49dd41b
王军 <>
2018-12-12 12:04:43 +0800

    增加swagger测试 and Jpa 测试

---
+++ cicoding-entity/src/main/java/com/cicoding/integration/User.java
@@ -0,0 +1,14 @@
+package com.cicoding.integration;
+
+/**
+ * com.cicoding.integration
+ * <strong>描述：</strong>
+ * <strong>功能：</strong>
+ * <strong>使用场景：</strong>
+ * <strong>注意事项：</strong>
+ *
+ * @author: wangjun
+ * @date: 2018/12/12.
+ */
+public class User {
+}
---
+++ cicoding-web/src/main/java/com/cicoding/integration/swagger2/Swagger2.java
@@ -0,0 +1,14 @@
+package com.cicoding.integration.swagger2;
+
+/**
+ * com.cicoding.integration.swagger2
+ * <strong>描述：</strong>
+ * <strong>功能：</strong>
+ * <strong>使用场景：</strong>
+ * <strong>注意事项：</strong>
+ *
+ * @author: wangjun
+ * @date: 2018/12/12.
+ */
+public class Swagger2 {
+}
